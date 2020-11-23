0a6bbe11505d21847f34859338b6d8be60a97514
BokunoMasayume <allenwalker233333@gmail.com>
2020-11-24 01:30:11 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -21,7 +21,7 @@ module.exports = {
     mode: "development",
     plugins: [
         // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
           title: 'Caching',
           filename: '../warmup.html'
